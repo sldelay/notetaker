5fa19c9cc9d721832974fa012e6cbe2bb01af221
Sam Delay <samuel.delay@gmail.com>
2020-03-15 14:45:23 -0400

    added api routes

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
@@ -20,6 +21,51 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public", "notes.html"));
 });
 
+app.get("/api/notes", function (reg, res) {
+    let obj = JSON.parse(fs.readFileSync('db/db.json', 'utf8'));
+    return res.json(obj);
+});
 
+app.post("/api/notes", function (req, res) {
+    let note = req.body;
+    fs.readFile('db/db.json', 'utf8', function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            obj = JSON.parse(data);
+            obj.push(note);
+            json = JSON.stringify(obj);
+            fs.writeFile('db/db.json', json, 'utf8', function (err, data) {
+                if (err) throw err;
+            });
+        }
+    });
+    return res.json(note);
+});
 
+app.delete('/api/notes/:id', function (req, res) {
+    function processDelete(noteID) {
+        fs.readFile('db/db.json', 'utf8', function (err, data) {
+            if (err) {
+                console.log(err);
+            } else {
+                obj = JSON.parse(data);
+                obj.forEach(el => {
+                    if (el.id === noteID) {
+                        let foundNote = obj.indexOf(el);
+                        obj.splice(foundNote, 1)
+                        json = JSON.stringify(obj);
+                        fs.writeFile('db/db.json', json, 'utf8', function (err, data) {
+                            if (err) throw err;
+                        });
+                    } else {
+                        return
+                    }
+                })
+            }
+        });
+    }
+    processDelete(req.params.id)
+    return res.send("Successfully deleted!")
+});
 
